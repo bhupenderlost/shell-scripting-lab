3f46bb217b10a08679b0d6873c47db9ecb73c607
= <=>
2023-02-16 13:58:51 +0530

    Lab 5

---
+++ string.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+string1="linux"
+string2="hint"
+
+echo "String1+String2"
+string3=$string1+$string2
+string3+="It's a complete string"
+echo $string3
---
+++ swapTwoNumbers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+num1=10
+num2=20
+
+echo "Before Swapping"
+echo "num1 = $num1"
+echo "num2 = $num2"
+
+num3=$num1
+num1=$num2
+num2=$num3
+
+echo "After Swapping"
+echo "num1 = $num1"
+echo "num2 = $num3"
